5e3ccd3f45508830ae2bec0440a2915ceb07a40b
Annie Sexton <anniesexton@Annies-M1.local>
2023-08-23 14:02:30 -0700

    whatever

--- src/components/WritingLink.tsx
+++ src/components/WritingLink.tsx
@@ -7,18 +7,16 @@ const WritingLink = ({thumbnail, title, subtitle, desc, src}:{
 }) => {
   function render() {
     return(
-      <div className="md:flex md:flex-col gap-10">
-        <a href={src} className="bg-white rounded-md shadow-md p-5 block mb-10 md:mb-0 md:flex md:gap-10 md:items-center hover:shadow-lg transition-all hover:-translate-y-1.5 hover:bg-gray-100">
-          <img src={thumbnail} alt="" width="313" className="mb-6 md:mb-0"/>
-          <div className="text-left">
-            <h3 className="text-2xl font-bold pb-3">{title}</h3>
-            {subtitle && 
-            <h4>{subtitle}</h4>
-            }
-            <p className="text-[16px] leading-6">{desc}</p>
-          </div>
-        </a>
-      </div>
+      <a href={src} className="bg-white border-gray-300 border rounded-md shadow-md p-5 block mb-10 md:mb-0 md:flex md:gap-10 md:items-center hover:shadow-lg transition-all hover:-translate-y-1.5 hover:bg-gray-100">
+        <img src={thumbnail} alt="" width="313" className="mb-6 md:mb-0"/>
+        <div className="text-left">
+          <h3 className="text-2xl font-bold pb-3">{title}</h3>
+          {subtitle && 
+          <h4>{subtitle}</h4>
+          }
+          <p className="text-[16px] leading-6">{desc}</p>
+        </div>
+      </a>
     )
   }
 
